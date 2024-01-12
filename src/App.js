cfdfde3c99c896ebc6055d7ceba3141aee252cb5
AYOUB EL MAKHTOUM <ayoubsamado2004@gmail.com>
2024-01-12 13:55:26 -0800

    Add Select Plan and Subsription Component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import Personalinfo from "./Components/Personal-Info/Personalinfo";
 import StepsNavigation from "./Components/Stepsnavigation/StepsNavigation";
 import "./App.css";
 import { reducer } from "./Components/reducer";
+import SelectPlan from "./Components/SelectPlan/SelectPlan.jsx";
 const initialValue = {
   info: {
     name: "",
     email: "",
     phone: "",
+  },
+  subscription: {
+    subscription: "Monthly",
+    type: "Arcade",
   },
   step: 1,
 };
@@ -26,6 +31,7 @@ function App() {
       <div className="container">
         <StepsNavigation />
         {data.step === 1 && <Personalinfo />}
+        {data.step === 2 && <SelectPlan />}
       </div>
     </Context.Provider>
   );
--- src/Components/reducer.js
+++ src/Components/reducer.js
@@ -1,13 +1,17 @@
 const ADD_INFO = "addInfo";
 const NEXT_STEP = "nextStep";
+const ADD_SUBSCRIPTION = "addSubscription";
 const reducer = (state, action) => {
   switch (action.type) {
     case ADD_INFO:
       return { ...state, info: action.payload.info };
-    case NEXT_STEP: return {...state, step: (state.step + action.payload.step)}
+    case NEXT_STEP:
+      return { ...state, step: state.step + action.payload.step };
+    case ADD_SUBSCRIPTION:
+      return { ...state, subscription: action.payload.subscription };
     default:
       return state;
   }
 };
 
-export {ADD_INFO, reducer, NEXT_STEP};
+export { ADD_INFO, reducer, NEXT_STEP, ADD_SUBSCRIPTION };
